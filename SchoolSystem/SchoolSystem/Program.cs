a2e9190d8d38f44738a1415f709e14cfb3605d80
Alex <alexgm19991@gmail.com>
2021-05-31 18:14:40 +0300

    creating class Student

---
+++ SchoolSystem/SchoolSystem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SchoolSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ SchoolSystem/SchoolSystem/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolSystem
+{
+    class Student
+    {
+        private string firstName;
+        private string lastName;
+        private int averageGrade;
+
+        public Student(string fName, string lName, int aGrade)
+        {
+            FirstName = fName;
+            LastName = lName;
+            AverageGrade = aGrade;
+        }
+
+        public int AverageGrade { get => averageGrade; set => averageGrade = value; }
+        public string LastName { get => lastName; set => lastName = value; }
+        public string FirstName { get => firstName; set => firstName = value; }
+    }
+}
