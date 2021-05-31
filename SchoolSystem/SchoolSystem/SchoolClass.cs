1f56dc795672dac7e251ff2441d2c4709a90adf9
Alex <alexgm19991@gmail.com>
2021-05-31 18:21:20 +0300

    creating SchoolClass class

--- SchoolSystem/SchoolSystem/SchoolClass.cs
+++ SchoolSystem/SchoolSystem/SchoolClass.cs
@@ -6,5 +6,16 @@ namespace SchoolSystem
 {
     class SchoolClass
     {
+        private char classID;
+        private string teacher;
+
+        public SchoolClass(char classID, string teacher)
+        {
+            ClassID = classID;
+            Teacher = teacher;
+        }
+
+        public char ClassID { get => classID; set => classID = value; }
+        public string Teacher { get => teacher; set => teacher = value; }
     }
 }
